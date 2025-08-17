9cfe5599599f57eaedfcd20237328d2410dc6c7e
ehrohange <jorgeangelopangilinan@gmail.com>
2025-08-17 22:55:50 +0800

    navbar ui finished

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -18,8 +18,11 @@ import {
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useState } from "react";
 
 const AppSideBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(true);
+
   return (
     <Sidebar className="md:hidden">
       <SidebarHeader>
@@ -31,82 +34,93 @@ const AppSideBar = () => {
             <SidebarTrigger />
           </header>
         </SidebarGroup>
-        <SidebarGroup className="hover:bg-primary/10 rounded-sm pt-0">
-          <SidebarGroupLabel className="p-0 mb-2">User</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <Link to={"/profile"} className="flex items-start gap-2">
-              <Avatar className="size-10">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col gap-0 pr-4 min-w-0 mt-[-4px]">
-                <h1 className="font-medium truncate">
-                  Jorge Angelo M. Pangilinan
-                </h1>
-                <p className="text-secondary truncate text-xs">
-                  jorgeangelopangilinan@gmail.com
-                </p>
-                <div className="w-full flex items-center justify-between text-secondary mt-2">
-                  <span className="text-xs">Edit Profile</span>
-                  <ChevronRight className="size-4" />
-                </div>
-              </div>
-            </Link>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup className="mt-[-10px] p-0">
-          <SidebarGroupLabel className="mb-2">User</SidebarGroupLabel>
-          <SidebarGroupContent className="flex flex-col gap-4 px-2">
-            <Link to={"/budget"}>
-              <Button className="w-full flex items-center justify-center">
-                <LogIn />
-                <span>Login</span>
-              </Button>
-            </Link>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup>
-          <hr />
-        </SidebarGroup>
+        {isLoggedIn ? (
+          <>
+            <SidebarGroup className="hover:bg-primary/10 rounded-sm pt-0">
+              <SidebarGroupLabel className="p-0 mb-2">User</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <Link to={"/profile"} className="flex items-start gap-2">
+                  <Avatar className="size-10">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <div className="flex flex-col gap-0 pr-4 min-w-0 mt-[-4px]">
+                    <h1 className="font-medium truncate">
+                      Jorge Angelo M. Pangilinan
+                    </h1>
+                    <p className="text-secondary truncate text-xs">
+                      jorgeangelopangilinan@gmail.com
+                    </p>
+                    <div className="w-full flex items-center justify-between text-secondary mt-2">
+                      <span className="text-xs">Edit Profile</span>
+                      <ChevronRight className="size-4" />
+                    </div>
+                  </div>
+                </Link>
+              </SidebarGroupContent>
+            </SidebarGroup>
+            <SidebarGroup>
+              <hr />
+            </SidebarGroup>
+          </>
+        ) : (
+          <SidebarGroup className="mt-[-10px] p-0">
+            <SidebarGroupLabel className="mb-2">User</SidebarGroupLabel>
+            <SidebarGroupContent className="flex flex-col gap-4 px-2">
+              <Link to={"/budget"}>
+                <Button className="w-full flex items-center justify-center">
+                  <LogIn />
+                  <span>Login</span>
+                </Button>
+              </Link>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
       </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup className="mt-[-10px]">
-          <SidebarGroupLabel className="mb-2">Application</SidebarGroupLabel>
-          <SidebarGroupContent className="flex flex-col gap-4 px-2">
-            <Link to={"/budget"}>
-              <Button className="w-full flex items-center justify-center">
-                <PlusCircle />
-                <span>Add Budget</span>
-              </Button>
-            </Link>
-            <Link to={"/dashboard"}>
-              <Button
-                className="w-full flex items-center justify-between"
-                variant={"ghost"}
-              >
-                <ChartSpline />
-                <span>Dashboard</span>
-                <ChevronRight />
-              </Button>
-            </Link>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter>
-        <Button
-          variant={"ghost"}
-          className="text-destructive bg-none border-destructive hover:!bg-destructive/10 hover:text-destructive hover:border-2
+      {isLoggedIn && (
+        <>
+          <SidebarContent>
+            <SidebarGroup className="mt-[-10px]">
+              <SidebarGroupLabel className="mb-2">
+                Application
+              </SidebarGroupLabel>
+              <SidebarGroupContent className="flex flex-col gap-4 px-2">
+                <Link to={"/budget"}>
+                  <Button className="w-full flex items-center justify-center">
+                    <PlusCircle />
+                    <span>Add Budget</span>
+                  </Button>
+                </Link>
+                <Link to={"/dashboard"}>
+                  <Button
+                    className="w-full flex items-center justify-between"
+                    variant={"ghost"}
+                  >
+                    <ChartSpline />
+                    <span>Dashboard</span>
+                    <ChevronRight />
+                  </Button>
+                </Link>
+              </SidebarGroupContent>
+            </SidebarGroup>
+            <SidebarGroup />
+          </SidebarContent>
+          <SidebarFooter>
+            <Button
+              variant={"ghost"}
+              className="text-destructive bg-none border-destructive hover:!bg-destructive/10 hover:text-destructive hover:border-2
           flex items-center justify-between"
-        >
-          <LogOut />
-          <span>Logout</span>
-          <ChevronRight />
-        </Button>
-      </SidebarFooter>
+            >
+              <LogOut />
+              <span>Logout</span>
+              <ChevronRight />
+            </Button>
+          </SidebarFooter>
+        </>
+      )}
     </Sidebar>
   );
 };
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,51 +1,102 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { SidebarTrigger } from "./ui/sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+import { NavigationMenuContent } from "@radix-ui/react-navigation-menu";
+import { LogIn, LogOut, PlusCircle } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(true);
+
   return (
-    <div className="w-full">
-      <nav className="bg-background text-foreground flex mx-auto items-center justify-between w-full max-w-6xl py-4 px-4">
+    <NavigationMenu className="w-full block max-w-none" viewport={false}>
+      <NavigationMenuList className="flex mx-auto items-center justify-between w-full max-w-6xl py-4 px-4">
         <SidebarTrigger className="md:hidden" />
-        <div className="flex items-center justify-center flex-1 md:flex-none">
-          <Link
-            to="/"
-            className="w-fit gap-1"
-          >
-            <Button variant={"ghost"} className="ml-[-20px] md:ml-0">
+        <NavigationMenuItem className="flex items-center justify-center flex-1 md:flex-none">
+          <NavigationMenuLink className="ml-[-20px] md:ml-0" asChild>
+            <Link to="/" className="w-fit gap-2 flex flex-row items-center">
               <span className="logo !bg-accent"></span>
               <h1 className="text-xl font-doto uppercase text-white font-bold">
                 FinTrack
               </h1>
               <span className="logo rotate-180 ml-[-2px] !bg-accent"></span>
-            </Button>
-          </Link>
-        </div>
-        <ul className="md:flex items-center space-x-4 hidden">
-          <li>
-            <Link to="/dashboard">
-              <Button className="text-white" variant={"ghost"}>
-                Dashboard
-              </Button>
             </Link>
-          </li>
-          <li>
-            <Link to="/profile">
-              <Button className="text-white" variant={"ghost"}>
-                Profile
-              </Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/login">
-              <Button className="text-white" variant={"default"}>
-                Login
-              </Button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuList className="md:flex items-center space-x-4 hidden">
+          {isLoggedIn ? (
+            <>
+              <NavigationMenuItem>
+                <Link to={"/"}>
+                  <Button>
+                    <PlusCircle />
+                    <span>Add Budget</span>
+                  </Button>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link to="/dashboard">Dashboard</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem className="relative">
+                <NavigationMenuTrigger className="flex flex-row items-center gap-2 w-[200px]">
+                  <Avatar className="size-8">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <div className="flex flex-col gap-0 pr-4 min-w-0 mt-[-2px]">
+                    <h1 className="font-medium text-sm truncate">
+                      Jorge Angelo M. Pangilinan
+                    </h1>
+                    <p className="text-secondary truncate text-xs">
+                      jorgeangelopangilinan@gmail.com
+                    </p>
+                  </div>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="absolute top-full right-0 mt-2 w-[200px] rounded-md bg-popover shadow-lg p-2">
+                  <ul className="grid gap-2">
+                    <li>
+                      <NavigationMenuLink asChild>
+                        <Link to={"/profile"} className="text-center">Edit Profile</Link>
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <Button
+                        variant={"ghost"}
+                        className="w-full text-destructive bg-none border-destructive hover:!bg-destructive/10 hover:text-destructive hover:border-2"
+                      >
+                        <LogOut />
+                        <span>Logout</span>
+                      </Button>
+                    </li>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </>
+          ) : (
+            <NavigationMenuItem>
+              <Link to="/login">
+                <Button className="text-white" variant={"default"}>
+                  <LogIn /><span>Login</span>
+                </Button>
+              </Link>
+            </NavigationMenuItem>
+          )}
+        </NavigationMenuList>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
 
