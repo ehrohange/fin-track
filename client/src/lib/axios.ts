7e8bd514cd3a3c75d4c7da5be84ea3bb07e8544d
ehrohange <jorgeangelopangilinan@gmail.com>
2025-09-29 22:52:14 +0800

    updated axios base url

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create(
     {
-        baseURL : `${import.meta.env.EXPRESS_BACKEND_BASE_URL}/api`,
+        baseURL : `${import.meta.env.VITE_EXPRESS_BACKEND_BASE_URL}/api`,
     }
 );
 
